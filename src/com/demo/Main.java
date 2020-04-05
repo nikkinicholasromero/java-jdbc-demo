97a12954b5d8d364aae2cc02801426b4fefd65c2
unknown <nikkinicholas.romero@gmail.com>
2020-04-05 02:28:45 -0400

    Added notes

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -5,6 +5,53 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.*;
 
 public class Main {
+    /*
+        JDBC URL Format = jdbc:${vendor}:${databaseSpecific}
+        Connection
+            public Statement createStatement();
+            public Statement createStatement(int type, int concurrency);
+
+            ResultSet.TYPE_FORWARD_ONLY         - default
+            ResultSet.TYPE_SCROLL_SENSITIVE     - forward/backward - data is static
+            ResultSet.TYPE_SCROLL_INSENSITIVE   - forward/backward - data is dynamic
+
+            ResultSet.CONCUR_READ_ONLY          - default
+            ResultSet.CONCUR_UPDATABLE          - allows you to use resultSet to update the database
+
+        Statement
+            public boolean execute(String statement) throws SQLException;
+            public ResultSet executeQuery(String query) throws SQLException;
+            public int executeUpdate(String command) throws SQLException;
+
+        ResultSet
+            public ResultSet getResultSet();
+            public int getUpdateCount();
+
+            public boolean next();
+            public boolean previous();
+            public boolean first();
+            public boolean last();
+            public boolean beforeFirst();
+            public boolean afterLast();
+            public boolean absolute(int rowNum);
+            public boolean relative(int rowNum);
+
+            public boolean getBoolean();
+            public int getInt();
+            public long getLong();
+            public double getDouble();
+            public java.sql.Date getDate();
+            public java.sql.Time getTime();
+            public java.sql.TimeStemp getTimeStamp();
+            public String getString();
+            public Object getObject();
+
+        SQLException
+            public String getMessage();
+            public String getSQLState();
+            public String getErrorCode();
+     */
+
     private static final String URL = "jdbc:mysql://localhost/demo";
     private static final String USER = "admin";
     private static final String PASS = "pass123";
